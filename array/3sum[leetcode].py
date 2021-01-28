491d850e1a6b375070ee259466feaba0350be301
Abhishek Singh <s.abhishek2399@gmail.com>
2021-01-28 14:23:15 +0530

    3 sum problem

---
+++ array/3sum[leetcode].py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def threeSum(self, nums):
+        nums.sort()
+        output=[]
+        for i in range(len(nums)-2):
+            if(i>0 and (nums[i]==nums[i-1])):
+                continue
+            j=i+1
+            k=len(nums)-1
+            while(j<k):
+                res=nums[i]+nums[j]+nums[k]
+                if res==0:
+                    output.append([nums[i],nums[j],nums[k]])
+                    while((j<k) and (nums[j]==nums[j+1])):
+                        j+=1
+                    while(j<k and (nums[k]==nums[k-1])):    
+                        k-=1
+                        
+                    j+=1
+                    k-=1
+                elif res<0:
+                    j+=1
+                else:
+                    k-=1
+        return output    
+                
