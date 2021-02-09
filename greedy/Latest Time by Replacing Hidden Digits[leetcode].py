9bacbb0458a6804ac5575cca0820808b3cfaaad0
Abhishek Singh <s.abhishek2399@gmail.com>
2021-02-09 14:42:57 +0530

    Latest Time by Replacing Hidden Digits

---
+++ greedy/Latest Time by Replacing Hidden Digits[leetcode].py
@@ -0,0 +1,20 @@
+class Solution:
+    def maximumTime(self, time: str) -> str:
+        time = list(time)
+        if time[0] == '?':
+            if time[1] in '456789':
+                time[0] = '1'
+            else:
+                time[0] = '2'
+        if time[1] == '?':
+            if time[0] == '2':
+                time[1] = '3'
+            else:
+                time[1] = '9'
+
+        if time[3] == '?':
+            time[3] = '5'
+        if time[4] == '?':
+            time[4] = '9'
+
+        return ''.join(time)
