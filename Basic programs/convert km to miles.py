ce338a29bc2ce2ce87c40d3b9faa016cad5004a7
Abhishek Singh <s.abhishek2399@gmail.com>
2021-02-16 10:45:08 +0530

    km to miles

---
+++ Basic programs/convert km to miles.py
@@ -0,0 +1,7 @@
+# 1 kilometer is equal to 0.62137 miles.
+
+#Miles = kilometer * 0.62137   
+#Kilometer = Miles / 0.62137   
+
+km=float(input('Enter the kilometers?:'))  
+print("To miles it is :", (km*0.62137))
