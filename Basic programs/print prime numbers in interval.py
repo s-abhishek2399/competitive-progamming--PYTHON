e66e62c3cd893666d514cfdc57ddbe7a90534ac8
Abhishek Singh <s.abhishek2399@gmail.com>
2021-02-16 11:45:50 +0530

    prime numer in range

---
+++ Basic programs/print prime numbers in interval.py
@@ -0,0 +1,9 @@
+start= int(input())
+end= int(input())
+for num in range(start, end+ 1):
+     if num > 1:
+       for i in range(2, num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)
